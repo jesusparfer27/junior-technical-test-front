9b5d629b2c5b270bc63d4296df6feb359828829c
jesusparfer27 <jesusparfer@alumnos.cei.es>
2024-11-08 21:26:12 +0100

    commit

--- src/pages/FurnitureScroll.jsx
+++ src/pages/FurnitureScroll.jsx
@@ -57,7 +57,7 @@ export const FurnitureScroll = () => {
                         <p>{furniture_description}</p>
                     </div>
                     <div className="flex-right">
-                        <img src={`${VITE_API}${VITE_MONGO_ENDPOINT}${image}`} alt="" />
+                        <img src={`${VITE_API}/${image}`} alt="" />
                     </div>
                 </div>
             </div>
